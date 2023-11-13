ddb46d8a0206ecbbef8daa96a9496f7fc5bdb167
zhengqilin <zhengqilin>
2023-11-13 16:25:06 +0800

    commodity

--- run.bat
+++ run.bat
@@ -8,7 +8,7 @@ python.exe -m pip install --upgrade pip
 .\venv\Scripts\pip.exe install -r requirements.txt
 
 if %errorlevel% equ 0 (
-   python.exe app.py --xformers --setup_mode%*
+   python.exe app.py --xformers --setup_mode --share%*
 )
 
 if %errorlevel% neq 0 (
