bb01fd2367ed1e8ddf4e1f1c4f876b21cf6b9ec3
zhengqilin <zhengqilin>
2024-01-09 18:06:12 +0800

    add DWPose, this commit log:3dca5db79d9f9ffdd378753ddf6ec66535aace88; https://github.com/IDEA-Research/DWPose.git

---
+++ repositories/DWPose/ControlNet-v1-1-nightly/dwpose_infer_example.py
@@ -0,0 +1,11 @@
+from annotator.dwpose import DWposeDetector
+
+if __name__ == "__main__":
+    pose = DWposeDetector()
+    import cv2
+    # test_image = 'test_imgs/pose1.png'
+    test_image = '/data1/aigc/phototrend/test/input/1.png'
+    oriImg = cv2.imread(test_image)  # B,G,R order
+    import matplotlib.pyplot as plt
+    out = pose(oriImg)
+    plt.imsave('result.jpg', out)
